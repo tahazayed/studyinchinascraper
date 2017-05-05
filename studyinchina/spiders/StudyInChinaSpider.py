5c5a3847948bc746cf26613b950739fdfb9200db
taha zayed <tahazayed@gmail.com>
2017-05-05 16:15:48 +0200

    2017-05-05

--- studyinchina/spiders/StudyInChinaSpider.py
+++ studyinchina/spiders/StudyInChinaSpider.py
@@ -16,77 +16,80 @@ class StudyInChinaSpider(scrapy.Spider):
     name = "studyinchinaspider"
     allowed_domains = ["www.csc.edu.cn"]
     base_url = 'http://www.csc.edu.cn/studyinchina/programsearchen.aspx?PageNo=%s'
-    pageid = 1524
+    pageid = 1
+    max_pageid = 1
     start_urls = [base_url % pageid]
 
 
     
 
     def parse(self, response):
+    
+        if self.max_pageid == 1:
+            self.max_pageid = int(Selector(response).xpath('//*[@id="zy_box"]/div[1]/text()').extract()[2].replace('/',''))
+        
         scholarships = Selector(response).xpath('//div[@class="zy_list blue"]')
 
         for scholarship in scholarships:
         
             item = ScholarshipItem()
             try:
-                item['University'] = scholarship.xpath('div[@class="mingcheng"]/a/text()').extract()[0]
+                item['University'] = scholarship.xpath('div[@class="mingcheng"]/a/text()').extract()[0].strip()
             except:
                 item['University'] = ''
                 pass
                 
             starred = ''
             try:
-                starred=scholarship.xpath('div[@class="zhuanye"]/font[@class="font-red"]/text()').extract()[0]
+                starred=scholarship.xpath('div[@class="zhuanye"]/font[@class="font-red"]/text()').extract()[0].strip()
             except:
                 pass 
                 
             try:
-                item['Program'] = starred + scholarship.xpath(
-                'div[@class="zhuanye"]/text()').extract()[0]
+                item['Program'] = starred + scholarship.xpath('div[@class="zhuanye"]/text()').extract()[0].strip()
             except:
                 item['Program'] = ''
                 pass  
                 
             try:
-                item['Degree'] = scholarship.xpath(
-                'div[@class="degree"]/text()').extract()[0]
+                item['Degree'] = scholarship.xpath('div[@class="degree"]/text()').extract()[0].strip()
             except:
                 item['Degree'] = ''
                 pass
                 
             try:                
-                item['Duration'] = scholarship.xpath('div[@class="xuezhi"]/text()').extract()[0]
+                item['Duration'] = scholarship.xpath('div[@class="xuezhi"]/text()').extract()[0].strip()
             except:
                 item['Duration'] = ''
                 pass
             
             try:            
-                item['Instruction_Language'] = scholarship.xpath('div[@class="yuyan"]/text()').extract()[0]
+                item['Instruction_Language'] = scholarship.xpath('div[@class="yuyan"]/text()').extract()[0].strip()
             except:
                 item['Instruction_Language'] = ''
                 pass
 
             try:                
-               item['Tuition_Fee_RMB'] = scholarship.xpath('div[@class="xuefei"]/text()').extract()[0]
+               item['Tuition_Fee_RMB'] = scholarship.xpath('div[@class="xuefei"]/text()').extract()[0].strip()
             except:
                 item['Tuition_Fee_RMB'] = ''
                 pass
 
             try:                
-                item['Starting_Date'] = scholarship.xpath('div[@class="rx_date"]/text()').extract()[0]
+                item['Starting_Date'] = scholarship.xpath('div[@class="rx_date"]/text()').extract()[0].strip()
             except:
                 item['Starting_Date'] = ''
                 pass
 
             try:
-                item['Application_Deadline'] = scholarship.xpath('div[@class="sq_date"]/text()').extract()[0]
+                item['Application_Deadline'] = scholarship.xpath('div[@class="sq_date"]/text()').extract()[0].strip()
             except:
                 item['Application_Deadline'] = ''
                 pass
                 
             yield item
             
-        if len(scholarships)>0:
+        if self.pageid < self.max_pageid:
             self.pageid = self.pageid + 1    
             next_page = self.base_url % self.pageid
             yield scrapy.Request(url=next_page, callback=self.parse,meta={'dont_merge_cookies': False},dont_filter=True,encoding='utf-8',errback=self.errback)    
